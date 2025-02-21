5fcd273f248bb7f356002da1fe0edaca3c1a105b
AndresRamirez917 <andresmintic2021@gmail.com>
2025-02-20 21:06:33 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -38,17 +38,23 @@ getdata2()
 const btn_validar = document.getElementById("btn-validar").onclick = (e) => {
     e.preventDefault();
     const namesArr = [
-        {id:"name", name:"Name"},
+        {id:"name", name:"Nombre"},
         {id:"email", name:"Email"},
         {id:"fecha", name:"Fecha"},
         {id:"hora", name:"Hora"},
-        {id:"mensaje", value:"mensaje"}
+        {id:"mensaje", name:"Mensaje"}
     ]
-    for (let {id,value} of namesArr) {
+    for (let {id,name} of namesArr) {
         const element = document.getElementById(id);
         if(element.value.trim() === ""){
             return swal({
                 title: `El campo ${name} no puede estar vacío`,
+                icon: "error",
+                 })
+        }
+        if(id === "email" && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(element.value)){
+            return swal({
+                title: `El campo ${name} no tiene el formato correcto`,
                 icon: "error",
                  })
         }
